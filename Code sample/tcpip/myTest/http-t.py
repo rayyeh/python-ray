25fbcaadd64dbd631a2a57999a045a69e852ba4a
yehrayyeh <yehrayyeh@localhost>
2012-11-27 14:08:12 +0800

    combind threadmix and httpserver

--- Code sample/tcpip/myTest/http-t.py
+++ Code sample/tcpip/myTest/http-t.py
@@ -4,13 +4,15 @@ import sys,cgi,httplib,random,logging
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from datetime import datetime
 from xml.etree import ElementTree
+import threading
+from SocketServer import ThreadingMixIn
 #import gevent.monkey; gevent.monkey.patch_all()
 
 
 #Define the HTTP handler that overrides do_POST
 class httpServHandler(BaseHTTPRequestHandler):
-    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
-    def do_POST(self):
+    
+    def do_POST(self):        
         #Parse the form data posted
         form = cgi.FieldStorage(
                 fp=self.rfile,
@@ -18,18 +20,12 @@ class httpServHandler(BaseHTTPRequestHandler):
                 environ={'REQUEST_METHOD':'POST',
                 'CONTENT_TYPE':self.headers['Content-Type'],
                                 })
-               
+        
         # Begin the response
         self.send_response(200)
         #self.send_header('Content-type','text-html')
         self.end_headers()
-
-        #send content
-        #self.wfile.write('Client: %s\n' % str(self.client_address))
-        #self.wfile.write('User-agent: %s\n' % str(self.headers['user-agent']))
-        #self.wfile.write('Path: %s\n' % self.path)
-        #self.wfile.write('Form data:\n')
-
+        
         # get form value and  send back to reponse
         for field in form.keys():
             field_item = form[field]
@@ -44,22 +40,25 @@ class httpServHandler(BaseHTTPRequestHandler):
         try:
             conn=httplib.HTTPConnection('127.0.0.1',8080)
         except Exception:
-            logging.error('connect SMS server fail')           
+            self.log_error('Send_SMS():Connect SMS server fail')            
+            sys.exit("some error message")
         
         SMS_text = '?'+'id='+str(random.randint(1, 1000))
+        
         try:
             conn.request('GET',SMS_text)
         except Exception:
-            logging.error('request SMS server fail:')
+            #self.log_error('Send_SMS():Request SMS server fail:%s',SMS_text)
+            self.log_message('Send_SMS():Request SMS server fail: IP=%s %s:',\
+                              self.address_string(),SMS_text)
             sys.exit("some error message")
 
         #get  response from server
         try:
             response=conn.getresponse()
         except Exception:
-            logging.error('get SMS server response fail')
-            sys.exit("some error message")
-            
+            self.log_error('Send_SMS():get SMS server response fail')
+            sys.exit("some error message")            
         
         #print '*** response.status:', response.status,'\tresponse reason:',response.reason
         data_received=response.read()
@@ -74,10 +73,12 @@ if __name__ == '__main__':
     #Set the root directory
     
     print 'Starting http server, use <Ctrl-C> to stop'
+    class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
+        pass
     
     #Create server object   
     server_address = ('127.0.0.1', 8000)
-    httpd = HTTPServer(server_address, httpServHandler)
+    httpd = ThreadingHTTPServer(server_address, httpServHandler)
     print('http server is running...')
     httpd.serve_forever()
      
---
+++ Code sample/tcpip/myTest/sms-t.py
@@ -0,0 +1,54 @@
+#-*-  coding :UTF-8  -*-
+
+from BaseHTTPServer import BaseHTTPRequestHandler
+import urlparse
+from datetime import datetime
+import random
+import threading
+from SocketServer import ThreadingMixIn
+#import gevent.monkey; gevent.monkey.patch_all()
+
+class GetHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = urlparse.urlparse(self.path)
+        message_parts = [
+            'CLIENT VALUES:',
+            'client_address=%s (%s)' % (self.client_address,
+                                        self.address_string()),
+            'command=%s' % self.command,
+            'path=%s' % self.path,
+            'real path=%s' % parsed_path.path,
+            'query=%s' % parsed_path.query,
+            'request_version=%s' % self.request_version,
+            '',
+            #'SERVER VALUES:',
+            #'server_version=%s' % self.server_version,
+            #'sys_version=%s' % self.sys_version,
+            #'protocol_version=%s' % self.protocol_version,
+            #'',
+            #'HEADERS RECEIVED:',
+            ]
+
+        self.send_response(200)
+        self.end_headers()
+        
+        msg1='<SEND> <TxnID>SENDMSG<SEND-RETN-DATE>'
+        msg2='</SEND-RETN-DATE>\
+                   <SEND-RETN-CODE>0000</SEND-RETN-CODE>\
+                   <SEND-RETN-CODE-DESC>Sucess</SEND-RETN-CODE-DESC>\
+                    </TxnID><SYSMSG><MSG-ID>'
+        msg3='</MSG-ID><MSG-DESC></MSG-DESC></SYSMSG></SEND>'    
+        d=datetime.now()
+        msg_datetime=str(d.strftime("%Y%m%d%H%m%S"))
+        msg_id=str(random.randint(1, 1000000))
+        message=msg1+msg_datetime+msg2+msg_id+msg3        
+        self.wfile.write(message)
+        return
+    
+if __name__ == '__main__':
+    from BaseHTTPServer import HTTPServer
+    class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
+        pass
+    server = ThreadingHTTPServer(('localhost', 8080), GetHandler)
+    print 'Starting SMS server, use <Ctrl-C> to stop'
+    server.serve_forever()
