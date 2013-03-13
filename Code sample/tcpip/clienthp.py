a720f962d40a403954c96c3d157585c7f4918809
yehrayyeh <yehrayyeh@gmail.com>
2013-03-13 11:29:23 +0800

    ????

---
+++ Code sample/tcpip/clienthp.py
@@ -0,0 +1,35 @@
+#######################################################
+## Demo:Opening a Client-Side Socket for Sending Data
+#######################################################
+print '#'*60
+print '# Demo:Opening a Client-Side Socket for Sending Data'
+print '#'*60
+
+import sys
+from socket import *
+
+serverHost = '192.168.110.93'
+serverPort = 15000
+
+message = ['Client Message1 come ', 'Client Message2 come']
+
+if len(sys.argv) > 1:
+    serverHost = sys.argv[1]
+
+#Create a socket
+sSock = socket(AF_INET, SOCK_STREAM)
+
+#Connect to server
+sSock.connect((serverHost, serverPort))
+
+#Send messages
+#for item in message:
+for i in range(1,20000):
+    item ='send from nonstop:'+str(i)
+    sSock.send(item)
+    data = sSock.recv(1024)
+    print 'Client received: ', data
+
+sSock.close()
+    
+
---
+++ Code sample/tcpip/myTest/readme.txt
@@ -0,0 +1,16 @@
+1.myTest 是實驗 Flask web framework 的folder
+2.交易curl1.py <---> http-flask.py  <----> sms-flask.py
+=============================================================
+3.httpserver.py - nativate python http server ,效能很慢
+4.http-thread - nativate python http server +thread mode,效能很慢
+5.http-flask.py -- flask+gevent +sqlite3
+6.http-tornado.py -- flask + tornado +sqlite3
+7.http-mongokit.py - 是flask+gevent+mongokit (使用mongodb)
+8.http-pymongo.py -- flask+gevent+pymongo (使用 mongodb)
+9.http-flask.py 要先執行 set ACSSMS_SETTINGS=settings.ini 
+============================================================
+8.smsserver.py -nativate python http server ,效能很慢
+9.sms-thread.py -nativate python http server +thread mode,效能很慢
+10.sms-flask.py - flask+gevent, 效能可以
+
+
